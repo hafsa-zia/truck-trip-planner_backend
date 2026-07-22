2b37c4d32d9e5ea75ebc5eaf4b0bb9ff7a95eb7c
hafsa-zia <hafsaz2533@gmail.com>
2026-07-23 01:21:25 +0500

    Add missing Django files and update project configuration

---
+++ logs/generator.py
@@ -0,0 +1,14 @@
+def generate_logs():
+
+    return [
+        {
+            "day": 1,
+            "driving": 11,
+            "off_duty": 10
+        },
+        {
+            "day": 2,
+            "driving": 9,
+            "off_duty": 10
+        }
+    ]
---
+++ trips/services.py
@@ -0,0 +1,10 @@
+def get_route_data(
+        current,
+        pickup,
+        dropoff
+):
+
+    return {
+        "distance": 1200,
+        "duration": 22
+    }
